bd5cecd454c35ad29b01afe94dc848db67f46b61
Thaanu Perera <thaanup@gmail.com>
2020-10-14 18:24:39 +0530

    firebase authentication completion
    Completion of the firebase authentication for the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{AngularFireModule} from '@angular/fire';
 import { AppComponent } from './app.component';
+import { FirebaseService } from './services/firebase.service';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAFJlV3QIUJfpg-8xWgC3VJJRI0Qdt9szw",
+      authDomain: "medihere-f0428.firebaseapp.com",
+      databaseURL: "https://medihere-f0428.firebaseio.com",
+      projectId: "medihere-f0428", 
+      storageBucket: "medihere-f0428.appspot.com",
+      messagingSenderId: "107884691371",
+      appId: "1:107884691371:web:68c4ad70fc2ec6c1b31cee",
+      measurementId: "G-FV67FH1TSF"
+    })
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  isLoggedIn = false
+  constructor(public firebaseAuth : AngularFireAuth) { }
+  async signin(email: string, password: string){
+
+    //* Sign In the user
+    await this.firebaseAuth.signInWithEmailAndPassword(email,password)
+    .then(res=>{
+      this.isLoggedIn = true
+      localStorage.setItem('user', JSON.stringify(res.user))
+    })
+  }
+
+  //* Sign Up the user
+  async signup(email: string, password: string){
+    await this.firebaseAuth.createUserWithEmailAndPassword(email,password)
+    .then(res=>{
+      this.isLoggedIn = true
+      localStorage.setItem('user', JSON.stringify(res.user))
+    })
+  }
+
+  //* Logout the user
+  logout(){
+    this.firebaseAuth.signOut()
+    localStorage.removeItem('user')
+  }
+
+}
